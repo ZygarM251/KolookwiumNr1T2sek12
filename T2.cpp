fe4e000dc46a940491cf0581702adbc88b06ab80
ZygarM251 <115568775+ZygarM251@users.noreply.github.com>
2024-02-05 12:56:37 +0100

    Add files via upload

--- T2.cpp
+++ T2.cpp
@@ -41,11 +41,11 @@ int main()
     Funktor fun(zbiorFunktora);
 
     // Przykładowa lista wartości do sprawdzenia
-    vector<double> valuesToCheck = { 3.0, 12.0, 5.5, 8.0, 15.0 };
+    vector<double> WartosciFunktora = { 3.0, 12.0, 5.5, 8.0, 15.0 };
 
     // Użycie funktora jako predykatu z algorytmem std::count_if
-    int countWithinBounds = count_if(valuesToCheck.begin(), valuesToCheck.end(), fun);
+    int ZliczoneWartosci = count_if(WartosciFunktora.begin(), WartosciFunktora.end(), fun);
 
     // Wyświetlenie wyniku
-    cout << "Liczba wartości w granicach zbioru: " << countWithinBounds << endl;
+    cout << "Liczba wartości w granicach zbioru: " << ZliczoneWartosci << endl;
 }
